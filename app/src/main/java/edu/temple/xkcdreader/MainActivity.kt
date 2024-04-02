2532d274baa0a8b58a8afc7d6788d94709b58bed
Eric Reizas <tuo17224@temple.edu>
2024-04-02 12:23:59 -0400

    Added requestBtn listener and intent handling for link view

--- app/src/main/java/edu/temple/xkcdreader/MainActivity.kt
+++ app/src/main/java/edu/temple/xkcdreader/MainActivity.kt
@@ -1,7 +1,11 @@
 package edu.temple.xkcdreader
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings.ACTION_APP_OPEN_BY_DEFAULT_SETTINGS
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
@@ -22,6 +26,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var titleTextView: TextView
     lateinit var altTextView: TextView
     lateinit var comicImageView: ImageView
+    lateinit var requestBtn : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,10 +37,27 @@ class MainActivity : AppCompatActivity() {
         titleTextView = findViewById(R.id.titleTextView)
         altTextView = findViewById(R.id.altTextView)
         comicImageView = findViewById(R.id.comicImageView)
+        requestBtn = findViewById(R.id.requestBtn)
 
         fetchComicButton.setOnClickListener{
             lifecycleScope.launch(Dispatchers.Main) {
                 fetchComic(comicNumberEditText.text.toString())
+            }
+        }
+        requestBtn.setOnClickListener(){
+            intent = Intent(ACTION_APP_OPEN_BY_DEFAULT_SETTINGS, Uri.parse("package:${packageName}"))
+            startActivity(intent)
+        }
+        //want to figure out which Intent started this activity to figure out what to do
+        intent.action?.run{
+            if(this == Intent.ACTION_VIEW){
+                //Now we know the Intent has the data we need
+                intent.data?.let{
+                    val path = it.path!!.substring(1,it.path!!.length-2)
+                    lifecycleScope.launch(Dispatchers.Main) {
+                        fetchComic(path)
+                    }
+                }
             }
         }
 
